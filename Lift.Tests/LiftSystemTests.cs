283e510aa8b8c51059bc7d10ebb6378b9f1ec978
Luis Felipe Diaz Valbuena <>
2025-10-30 16:23:48 -0500

    🔴 LiftSystem_LiftSystem_ShouldCloseAndMoveInTwoDifferentMoments

--- Lift.Tests/LiftSystemTests.cs
+++ Lift.Tests/LiftSystemTests.cs
@@ -107,6 +107,23 @@ public class LiftSystemUnitTests
     }
 
     [Fact]
+    public void LiftSystem_LiftSystem_ShouldCloseAndMoveInTwoDifferentMoments()
+    {
+        var lift = new Lift();
+        var system = new LiftSystem(lift);
+        
+        system.Request(2);
+        system.Tick();
+        system.Tick();
+        system.Request(5);
+        system.Tick();
+        system.Tick();
+        
+        lift.CurrentFloor.Should().Be(3);
+        lift.AreDoorsOpen.Should().BeFalse();
+    }
+
+    [Fact]
     public void LiftSystem_ShouldDoNothing_When_FinishARequest_And_ThereIsNoMoreRequest()
     {
         var lift = new Lift();
